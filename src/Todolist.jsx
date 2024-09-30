a51cab8311c10b38734042810f92004bb88b8692
MarkusMant <mantchaam@gmail.com>
2024-09-30 09:21:12 +0300

    Added row selection and handleDelete function

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
 import "ag-grid-community/styles/ag-theme-material.css"; // Optional Theme applied to the Data Grid
@@ -9,7 +9,9 @@ function TodoList() {
         date: "",
         priority: ""
     });
+
     const [todos, setTodos] = useState([]);
+    const gridRef = useRef();
     const [colDefs, setColDefs] = useState([
         {
             field: "description", filter: true, floatingFilter: true, animateRow: true,
@@ -33,9 +35,11 @@ function TodoList() {
             setDesc({ description: "", date: "", priority: "" });
         }
     }
-    const handleDelete = (row) => {
-        setTodos(todos.filter((desc, index) => index != row));
-
+    const handleDelete = () => {
+        gridRef.current.getSelectedNodes().length > 0 ?
+            setTodos(todos.filter((todo, index) =>
+                index != gridRef.current.getSelectedNodes()[0].id))
+            : alert('Select a row first!');
     }
 
     return (
@@ -59,10 +63,17 @@ function TodoList() {
                 onChange={e => setDesc({ ...desc, date: e.target.value })}
             />
             <button onClick={handleAdd}>Add Todo</button>
-            <div className="ag-theme-material" style={{ height: 500, width: "110%" }}>
+            <button onClick={handleDelete}>Delete</button>
+            <div className="ag-theme-material"
+                style={{
+                    height: 500, width: 600
+                }}>
                 <AgGridReact
+                    ref={gridRef}
                     rowData={todos}
                     columnDefs={colDefs}
+                    selection='singleRow'
+                    onGridReady={params => gridRef.current = params.api}
                 />
             </div >
         </>
